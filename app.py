5db8ffa6afe5c04e33f7c12bbc18cf08e7f3498b
Chuck Lien <chuck.lien@gmail.com>
2024-08-26 01:43:55 +0800

    add app.py and ignore

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from streamlit_cookies_controller import CookieController
+
+st.set_page_config("Cookie QuickStart", "🍪", layout="wide")
+
+controller = CookieController()
+
+# Set a cookie
+controller.set("cookie_name", "testing")
+st.write(st.session_state)
+
+# Get all cookies
+cookies = controller.getAll()
+st.write(cookies)
+
+# Get a cookie
+cookie = controller.get("cookie_name")
+st.write(cookie)
+
+# Remove a cookie
+controller.remove("cookie_name")
+st.write(st.session_state)
